6c62f2c46ff07a5d4d121d0fd0308d52698b2a6e
eleven-dimension <3096152319@qq.com>
2023-06-02 22:10:20 +0800

    Finish pattern.h

--- include/pattern.h
+++ include/pattern.h
@@ -4,36 +4,106 @@
 #include <algorithm>
 #include <array>
 #include <vector>
+#include <functional>
 
 #include "../include/board.h"
 
-template<class T, size_t... Ns> struct foo;
-
-template<class T, size_t N0, size_t... Ns>
-struct foo<T, N0, Ns...> {
-    using type = std::array<typename foo<T, Ns...>::type, N0>;
+template <size_t N>
+struct MultiArrayHelper
+{
+    template <class T>
+    constexpr std::array<T, N> operator*(T);
 };
 
-template<class T>
-struct foo<T> {
-    using type = T;
+template <class T, size_t... Ns>
+using MultiArray = decltype((MultiArrayHelper<Ns>{} * ... * T{}));
+
+template <class T>
+struct StdArrayTraiter : std::false_type
+{
 };
 
-template<class T, size_t... Ns>
-using multiarray_t = typename foo<T, Ns...>::type;
+template <class T, size_t N>
+struct StdArrayTraiter<std::array<T, N>> : std::true_type
+{
+    static constexpr size_t kSize = N;
+};
 
+template <class T, typename = void>
+struct MultiArrayExpander
+{
+    using ElementType = T;
+
+    static constexpr size_t kDimension = 0;
+};
+
+template <class T>
+struct MultiArrayExpander<T, std::enable_if_t<StdArrayTraiter<T>::value>>
+{
+    using ElementExpander =
+        MultiArrayExpander<std::remove_reference_t<decltype(std::declval<T>()[0])>>;
+
+    using ElementType = typename ElementExpander::ElementType;
+
+    static constexpr size_t kSize = StdArrayTraiter<T>::kSize;
+    static constexpr size_t kDimension = ElementExpander::kDimension + 1;
+};
+
+template <class T, class Index, class... Args>
+constexpr decltype(auto) MultiArrayAt(T &array, Index index, Args... indexes)
+{
+    using Expander = MultiArrayExpander<T>;
+    static_assert((sizeof...(Args) + 1) == Expander::kDimension);
+    static_assert(std::is_convertible_v<Index, size_t>);
+
+    if constexpr (sizeof...(Args))
+    {
+        return MultiArrayAt(array[index], indexes...);
+    }
+    else
+    {
+        return array[index];
+    }
+}
+
+template <class T>
+constexpr void MultiArrayForEach(
+    T &array, const std::function<void(typename MultiArrayExpander<T>::ElementType &)> &operation)
+{
+    if (!operation)
+    {
+        return;
+    }
+
+    using Expander = MultiArrayExpander<T>;
+
+    if constexpr (Expander::kDimension)
+    {
+        for (auto &element : array)
+        {
+            MultiArrayForEach(element, operation);
+        }
+    }
+    else
+    {
+        operation(array);
+    }
+}
 
 class Pattern
 {
 public:
-    Pattern(std::vector<int> chosen_positions);
+    Pattern(std::array<int, 6> basic_pattern_positions);
 
-    float get_pattern_value(Board& board);
+    float get_pattern_value(Board &board);
 
-    void update_pattern_value(Board& board, float target_value, float learning_rate);
+    float update_pattern_value(Board &board, float adjust);
 
     // member variables
-    multiarray_t<float, 12, 12, 12, 12, 12, 12> table;
+    MultiArray<float, 12, 12, 12, 12, 12, 12> table;
 
-    std::vector<int> chosen_positions;
+    std::array<std::array<int, 6>, 8> pattern_positions;
+    
+    void save_table(std::string path);
+    void load_table(std::string path);
 };
